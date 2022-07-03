04a9e31df92285bfd14da9696b25b53077545dd9
KingsOg <nft33@protonmail.com>
2022-07-03 12:31:47 +0100

    heroku hosting

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.get("/get/:id", async (request, response) => {
 })
 
 //setting up the server
-app.listen(process.env.PORT || PORT, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log(`Server is running.`)
 })
