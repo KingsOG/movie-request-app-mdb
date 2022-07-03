542aef577dbb2ef9cde16ac1cb06ce64dff6d60d
KingsOg <nft33@protonmail.com>
2022-07-03 22:47:44 +0100

    added a public and view folder

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ MongoClient.connect(dbConnectionStr)
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
-app.set('view engine', 'html')
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 // //app requests and responses
