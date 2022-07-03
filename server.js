62227beef3a2e350bc95f788e0adef341a0a0f22
KingsOg <nft33@protonmail.com>
2022-07-03 12:31:22 +0100

    heroku hosting

--- server.js
+++ server.js
@@ -26,9 +26,6 @@ MongoClient.connect(dbConnectionStr)
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('public'))
-app.use(bodyParser.json())
-app.set('view engine', 'ejs')
 
 // //app requests and responses
 
